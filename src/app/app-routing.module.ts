79067f1656724dfe17f3ecf7e3e95a4ab59a86f8
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-08 23:42:40 +0100

    components for reactive forms set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customers/customer/customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CustomerComponent },
+  { path: '**', component: CustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
